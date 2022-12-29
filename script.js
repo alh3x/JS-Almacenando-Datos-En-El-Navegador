a1d234155e1a4900a6b2c20b871a7dea02c11366
Oliver <oliver-pinto@outlook.com>
2022-12-29 20:20:59 -0300

    aula 01 agregando fecha a lista de tareas

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ import deleteIcon from './components/deleteIcon.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
+
+const addTask = (evento) => {
+  const list = document.querySelector('[data-list]');
+  const task = createTask(evento)
+  list.appendChild(task);
+
+}
+
+
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector("[data-form-date]");//1-captura el elemento input date
   const value = input.value;
-  const list = document.querySelector('[data-list]');
+  const date = calendar.value;//2-captura el valor del elemento input date
+  const dateFormat = moment(date).format("DD/MM/YYYY");//3-le da un formato estalecido mediante una libreria API
+  console.log(dateFormat);
   const task = document.createElement('li');
   task.classList.add('card');
   input.value = '';
@@ -21,10 +33,14 @@ const createTask = (evento) => {
   taskContent.appendChild(titleTask);
   // task.innerHTML = content;
 
+  const dateElement = document.createElement("span");//4-crea un elemnto html span
+  dateElement.innerHTML = dateFormat;//5-el elemento span recive valor y formato del paso 3 
+  console.log(dateElement)
   task.appendChild(taskContent);
+  task.appendChild(dateElement);//inserta el elemnto span del paso 5 y 4 como hijo el elemento task 
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 };
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+btn.addEventListener('click', addTask);
